9e46df2d6423bb75075d9fc0550ba728e804ef20
zzmp <zmp@umich.edu>
2014-06-25 20:56:51 -0700

    (feat) Serve core docs.

---
+++ src/serve_docs.rs
@@ -0,0 +1,21 @@
+extern crate iron;
+extern crate staticfile;
+extern crate mount;
+
+use std::io::net::ip::Ipv4Addr;
+use iron::{Iron, ServerT, Chain};
+use staticfile::Static;
+use mount::Mount;
+
+fn main() {
+    let mut server: ServerT = Iron::new();
+    // Serve core documentation
+    server.chain.link(Mount::new("/core", Static::new(Path::new("core/doc/"))));
+    // Serve core's index.html (from iron)
+    server.chain.link(Mount::new("/core", Static::new(Path::new("core/doc/iron/"))));
+    // Serve Iron documentation
+    server.chain.link(Static::new(Path::new("iron/doc/")));
+    // Serve Iron's index.html
+    server.chain.link(Static::new(Path::new("iron/doc/iron/")));
+    server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
+}
