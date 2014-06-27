8f1f61a1bea18fc19724c8444c417fbb936015ea
zzmp <zmp@umich.edu>
2014-06-27 06:29:44 +0000

    (fix) Isolated doc gen from execution.

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,42 @@
-# Update Rust
-make nightly-uninstall
-make nightly-install
+updateIron() {
+  rm -rf $1
+  git clone "https://github.com/reem/$1.git"
+  cd $1
+  cargo build -uv
+  rustdoc -L target/deps -o ../doc src/lib.rs
+  cd ..
+}
 
-# Update Cargo
+updateIronDoc() {
+  rm -rf $1
+  git clone "https://github.com/iron/$1.git"
+  cd $1
+  cargo build -v
+  rustdoc -L target/deps -o ../doc src/lib.rs
+  cd ..
+}
 
 # Generate Iron documentation
 mkdir -p iron
 cd iron
-./../updateIronDoc iron
+updateIron iron
 cd ..
 
 # Generate core documentation
-mkdir -p core
+mkdir -p ./core
 cd core
-./../updateIronDoc iron
-./../updateIronDoc urlencoded
-./../updateIronDoc logger
-./../updateIronDoc router
-./../updateIronDoc mount
-./../updateIronDoc persistent
-./../updateIronDoc static-file
-./../updateIronDoc session
-./../updateIronDoc cookie
-./../updateIronDoc body-parser
+updateIron iron
+updateIronDoc urlencoded
+updateIronDoc logger
+updateIronDoc router
+updateIronDoc mount
+updateIronDoc persistent
+updateIronDoc static-file
+updateIronDoc session
+updateIronDoc cookie
+updateIronDoc body-parser
+pwd
 cd ..
 
 # Serve documentation
 cargo build -u
-./target/serve_docs
